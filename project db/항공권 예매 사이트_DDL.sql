429558cdb8a9c30b8296c1d40ec4ad80a130e093
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-09-05 18:55:19 +0900

    DDL 수정

--- project db/항공권 예매 사이트_DDL.sql
+++ project db/항공권 예매 사이트_DDL.sql
@@ -7,7 +7,7 @@ USE RESERVATION;
 DROP TABLE IF EXISTS `member`;
 
 CREATE TABLE `member` (
-	`me_id`	varchar(20)	NOT NULL	COMMENT '영문으로 시작, 영문, 숫자 조합으로 10~20자',
+	`me_id`	varchar(20)	NOT NULL	PRIMARY KEY	COMMENT '영문으로 시작, 영문, 숫자 조합으로 10~20자',
 	`me_pw`	varchar(25)	NOT NULL	COMMENT '영문, 숫자, !@#$ 조합으로 12~25자',
 	`me_name`	varchar(30)	NOT NULL,
 	`me_eng_name`	varchar(30)	NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `member` (
 DROP TABLE IF EXISTS `point`;
 
 CREATE TABLE `point` (
-	`po_num`	int	NOT NULL	COMMENT 'AUTO_INCREMENT',
+	`po_num`	int	NOT NULL	PRIMARY KEY	AUTO_INCREMENT,
 	`po_me_id`	varchar(20)	NOT NULL,
 	`po_al_name`	varchar(20)	NOT NULL,
 	`po_mc_name`	varchar(10)	NOT NULL	COMMENT '누적마일리지로 결정',
@@ -31,7 +31,7 @@ CREATE TABLE `point` (
 DROP TABLE IF EXISTS `ticketing`;
 
 CREATE TABLE `ticketing` (
-	`ti_num`	int	NOT NULL	COMMENT 'AUTO_INCREMENT',
+	`ti_num`	int	NOT NULL	PRIMARY KEY	AUTO_INCREMENT,
 	`ti_sk_num`	int	NOT NULL,
 	`ti_me_id`	varchar(20)	NOT NULL,
 	`ti_amount`	int	NOT NULL	DEFAULT 1,
@@ -45,7 +45,7 @@ CREATE TABLE `ticketing` (
 DROP TABLE IF EXISTS `ticketting_list`;
 
 CREATE TABLE `ticketting_list` (
-	`tl_num`	varchar(20)	NOT NULL	COMMENT '예매일자(6), 출발지(3), 도착지(3), 예매순서(4), 좌석번호(4) -빈자리는 0으로 채운다',
+	`tl_num`	varchar(20)	NOT NULL	PRIMARY KEY	COMMENT '예매일자(6), 출발지(3), 도착지(3), 예매순서(4), 좌석번호(4) -빈자리는 0으로 채운다',
 	`tl_ti_num`	int	NOT NULL,
 	`tl_se_num`	int	NOT NULL
 );
@@ -53,7 +53,7 @@ CREATE TABLE `ticketting_list` (
 DROP TABLE IF EXISTS `schedule`;
 
 CREATE TABLE `schedule` (
-	`sk_num`	int	NOT NULL	COMMENT 'AUTO_INCREMENT',
+	`sk_num`	int	NOT NULL	PRIMARY KEY	AUTO_INCREMENT,
 	`sk_ap_num`	varchar(7)	NOT NULL,
 	`sk_ro_num`	int	NOT NULL,
 	`sk_stat_time`	datetime	NOT NULL,
@@ -65,16 +65,16 @@ CREATE TABLE `schedule` (
 DROP TABLE IF EXISTS `airport`;
 
 CREATE TABLE `airport` (
-	`ai_num`	varchar(3)	NOT NULL	COMMENT 'IATA코드',
-	`ai_name`	varchar(20)	NOT NULL,
-	`ai_nation`	varchar(10)	NOT NULL,
-	`ai_standard_time`	int	NOT NULL	COMMENT 'UTC'
+	`ai_num`	varchar(3)	NOT NULL	PRIMARY KEY	COMMENT 'IATA코드',
+	`ai_name`	varchar(30)	NOT NULL,
+	`ai_na_name`	varchar(20)	NOT NULL,
+	`ai_standard_time`	time	NOT NULL	COMMENT 'UTC'
 );
 
 DROP TABLE IF EXISTS `airplane`;
 
 CREATE TABLE `airplane` (
-	`ap_num`	varchar(7)	NOT NULL	COMMENT '국가코드(최대3자리)+등록번호(0000~9999)',
+	`ap_num`	varchar(7)	NOT NULL	PRIMARY KEY	COMMENT '국가코드(최대3자리)+등록번호(0000~9999)',
 	`ap_al_name`	varchar(20)	NOT NULL,
 	`ap_am_model`	varchar(10)	NOT NULL
 );
@@ -82,13 +82,13 @@ CREATE TABLE `airplane` (
 DROP TABLE IF EXISTS `airline`;
 
 CREATE TABLE `airline` (
-	`al_name`	varchar(20)	NOT NULL
+	`al_name`	varchar(20)	NOT NULL	PRIMARY KEY
 );
 
 DROP TABLE IF EXISTS `seat`;
 
 CREATE TABLE `seat` (
-	`se_num`	int	NOT NULL	COMMENT 'AUTO_INCREMENT',
+	`se_num`	int	NOT NULL	PRIMARY KEY	AUTO_INCREMENT,
 	`se_name`	varchar(4)	NOT NULL	COMMENT '영문2, 숫자2',
 	`se_sc_name`	varchar(20)	NOT NULL	DEFAULT '일반석',
 	`se_am_model`	varchar(10)	NOT NULL
@@ -97,16 +97,16 @@ CREATE TABLE `seat` (
 DROP TABLE IF EXISTS `airplane_model`;
 
 CREATE TABLE `airplane_model` (
-	`am_model`	varchar(4)	NOT NULL	COMMENT 'ICAO코드',
-	`am_first_class`	int	NOT NULL	DEFAULT 0,
-	`am_business_class`	int	NOT NULL	DEFAULT 0,
-	`am_economy_class`	int	NOT NULL	DEFAULT 0
+	`am_model`	varchar(4)	NOT NULL	PRIMARY KEY	COMMENT 'ICAO코드',
+	`am_name`	varchar(20)	NOT NULL,
+	`am_col`	int	NOT NULL	COMMENT '좌석 열 끝 번호',
+	`am_exit_col`	int	NOT NULL	COMMENT '출구가 있는 열 번호'
 );
 
 DROP TABLE IF EXISTS `route`;
 
 CREATE TABLE `route` (
-	`ro_num`	int	NOT NULL	COMMENT 'AUTO_INCREMENT',
+	`ro_num`	int	NOT NULL	PRIMARY KEY	AUTO_INCREMENT,
 	`ro_ai_start`	varchar(3)	NOT NULL,
 	`ro_ai_end`	varchar(3)	NOT NULL
 );
@@ -114,14 +114,14 @@ CREATE TABLE `route` (
 DROP TABLE IF EXISTS `seat_class`;
 
 CREATE TABLE `seat_class` (
-	`sc_name`	varchar(20)	NOT NULL	COMMENT '이코노미, 비지니스, 퍼스트',
+	`sc_name`	varchar(20)	NOT NULL	PRIMARY KEY	COMMENT '이코노미, 비지니스, 퍼스트',
 	`sc_ratio`	int	NOT NULL	COMMENT '1, 2, 4'
 );
 
 DROP TABLE IF EXISTS `point_history`;
 
 CREATE TABLE `point_history` (
-	`ph_num`	int	NOT NULL	COMMENT 'AUTO_INCREMENT',
+	`ph_num`	int	NOT NULL	PRIMARY KEY	AUTO_INCREMENT,
 	`ph_po_num`	int	NOT NULL,
 	`ph_ti_num`	int	NOT NULL,
 	`ph_detail`	varchar(20)	NOT NULL	COMMENT '적립, 사용 중 선택'
@@ -130,63 +130,139 @@ CREATE TABLE `point_history` (
 DROP TABLE IF EXISTS `member_class`;
 
 CREATE TABLE `member_class` (
-	`mc_name`	varchar(10)	NOT NULL	COMMENT '실버, 골드, 다이아몬드, 다이아몬드플러스, 플래티넘',
+	`mc_name`	varchar(10)	NOT NULL	PRIMARY KEY	COMMENT '실버, 골드, 다이아몬드, 다이아몬드플러스, 플래티넘',
 	`mc_bonus_point`	int	NOT NULL	COMMENT '1, 1.05, 1.1, 1.15, 1.2'
 );
 
-ALTER TABLE `member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
+DROP TABLE IF EXISTS `nation`;
+
+CREATE TABLE `nation` (
+	`na_name`	varchar(20)	NOT NULL	 PRIMARY KEY,
+	`na_division`	varchar(10)	NULL
+);
+
+ALTER TABLE `point` ADD CONSTRAINT `FK_member_TO_point_1` FOREIGN KEY (
+	`po_me_id`
+)
+REFERENCES `member` (
 	`me_id`
 );
 
-ALTER TABLE `point` ADD CONSTRAINT `PK_POINT` PRIMARY KEY (
-	`po_num`
-);
-
-ALTER TABLE `ticketing` ADD CONSTRAINT `PK_TICKETING` PRIMARY KEY (
-	`ti_num`
-);
-
-ALTER TABLE `ticketting_list` ADD CONSTRAINT `PK_TICKETTING_LIST` PRIMARY KEY (
-	`tl_num`
-);
-
-ALTER TABLE `schedule` ADD CONSTRAINT `PK_SCHEDULE` PRIMARY KEY (
-	`sk_num`
-);
-
-ALTER TABLE `airport` ADD CONSTRAINT `PK_AIRPORT` PRIMARY KEY (
-	`ai_num`
-);
-
-ALTER TABLE `airplane` ADD CONSTRAINT `PK_AIRPLANE` PRIMARY KEY (
-	`ap_num`
-);
-
-ALTER TABLE `airline` ADD CONSTRAINT `PK_AIRLINE` PRIMARY KEY (
+ALTER TABLE `point` ADD CONSTRAINT `FK_airline_TO_point_1` FOREIGN KEY (
+	`po_al_name`
+)
+REFERENCES `airline` (
 	`al_name`
 );
 
-ALTER TABLE `seat` ADD CONSTRAINT `PK_SEAT` PRIMARY KEY (
-	`se_num`
-);
-
-ALTER TABLE `airplane_model` ADD CONSTRAINT `PK_AIRPLANE_MODEL` PRIMARY KEY (
-	`am_model`
-);
-
-ALTER TABLE `route` ADD CONSTRAINT `PK_ROUTE` PRIMARY KEY (
-	`ro_num`
-);
-
-ALTER TABLE `seat_class` ADD CONSTRAINT `PK_SEAT_CLASS` PRIMARY KEY (
-	`sc_name`
-);
-
-ALTER TABLE `point_history` ADD CONSTRAINT `PK_POINT_HISTORY` PRIMARY KEY (
-	`ph_num`
-);
-
-ALTER TABLE `member_class` ADD CONSTRAINT `PK_MEMBER_CLASS` PRIMARY KEY (
+ALTER TABLE `point` ADD CONSTRAINT `FK_member_class_TO_point_1` FOREIGN KEY (
+	`po_mc_name`
+)
+REFERENCES `member_class` (
 	`mc_name`
 );
 
+ALTER TABLE `ticketing` ADD CONSTRAINT `FK_schedule_TO_ticketing_1` FOREIGN KEY (
+	`ti_sk_num`
+)
+REFERENCES `schedule` (
+	`sk_num`
+);
+
+ALTER TABLE `ticketing` ADD CONSTRAINT `FK_member_TO_ticketing_1` FOREIGN KEY (
+	`ti_me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `ticketting_list` ADD CONSTRAINT `FK_ticketing_TO_ticketting_list_1` FOREIGN KEY (
+	`tl_ti_num`
+)
+REFERENCES `ticketing` (
+	`ti_num`
+);
+
+ALTER TABLE `ticketting_list` ADD CONSTRAINT `FK_seat_TO_ticketting_list_1` FOREIGN KEY (
+	`tl_se_num`
+)
+REFERENCES `seat` (
+	`se_num`
+);
+
+ALTER TABLE `schedule` ADD CONSTRAINT `FK_airplane_TO_schedule_1` FOREIGN KEY (
+	`sk_ap_num`
+)
+REFERENCES `airplane` (
+	`ap_num`
+);
+
+ALTER TABLE `schedule` ADD CONSTRAINT `FK_route_TO_schedule_1` FOREIGN KEY (
+	`sk_ro_num`
+)
+REFERENCES `route` (
+	`ro_num`
+);
+
+ALTER TABLE `airport` ADD CONSTRAINT `FK_nation_TO_airport_1` FOREIGN KEY (
+	`ai_na_name`
+)
+REFERENCES `nation` (
+	`na_name`
+);
+
+ALTER TABLE `airplane` ADD CONSTRAINT `FK_airline_TO_airplane_1` FOREIGN KEY (
+	`ap_al_name`
+)
+REFERENCES `airline` (
+	`al_name`
+);
+
+ALTER TABLE `airplane` ADD CONSTRAINT `FK_airplane_model_TO_airplane_1` FOREIGN KEY (
+	`ap_am_model`
+)
+REFERENCES `airplane_model` (
+	`am_model`
+);
+
+ALTER TABLE `seat` ADD CONSTRAINT `FK_seat_class_TO_seat_1` FOREIGN KEY (
+	`se_sc_name`
+)
+REFERENCES `seat_class` (
+	`sc_name`
+);
+
+ALTER TABLE `seat` ADD CONSTRAINT `FK_airplane_model_TO_seat_1` FOREIGN KEY (
+	`se_am_model`
+)
+REFERENCES `airplane_model` (
+	`am_model`
+);
+
+ALTER TABLE `route` ADD CONSTRAINT `FK_airport_TO_route_1` FOREIGN KEY (
+	`ro_ai_start`
+)
+REFERENCES `airport` (
+	`ai_num`
+);
+
+ALTER TABLE `route` ADD CONSTRAINT `FK_airport_TO_route_2` FOREIGN KEY (
+	`ro_ai_end`
+)
+REFERENCES `airport` (
+	`ai_num`
+);
+
+ALTER TABLE `point_history` ADD CONSTRAINT `FK_point_TO_point_history_1` FOREIGN KEY (
+	`ph_po_num`
+)
+REFERENCES `point` (
+	`po_num`
+);
+
+ALTER TABLE `point_history` ADD CONSTRAINT `FK_ticketing_TO_point_history_1` FOREIGN KEY (
+	`ph_ti_num`
+)
+REFERENCES `ticketing` (
+	`ti_num`
+);
