8febb6edce85a827c0e85916ebfbcbbff89340a2
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-09-12 00:28:52 +0900

    admin오타 수정

--- project/src/main/java/kr/kh/project/controller/AdminController.java
+++ project/src/main/java/kr/kh/project/controller/AdminController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdmniController {
+public class AdminController {
 	
 	
 	@GetMapping("/menu/admin")
 	public String menuAdmin() {
+		
 		return "/menu/admin";
 	}
 }
