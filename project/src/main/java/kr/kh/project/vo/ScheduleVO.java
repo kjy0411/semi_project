2e1eac8b159fe17020ecd46924d6c84c1379abba
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-09-12 15:58:11 +0900

    VO 수정

--- project/src/main/java/kr/kh/project/vo/ScheduleVO.java
+++ project/src/main/java/kr/kh/project/vo/ScheduleVO.java
@@ -1,5 +1,6 @@
 package kr.kh.project.vo;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,20 +16,36 @@ public class ScheduleVO {
 	Date sk_time;
 	int sk_price;
 	
-	public String getAi_standard_time_str() {
+	public String getSk_start_time_str() {
 		if(sk_start_time == null) {
 			return "";	
 		}
-		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		return format.format(sk_start_time);
+	}
+	public void setSk_start_time(String time) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			sk_start_time = format.parse(time);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getSk_end_time_str() {
 		if(sk_end_time == null) {
 			return "";	
 		}
-		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		return format.format(sk_end_time);
+	}
+	public void setSk_end_time(String time) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			sk_end_time = format.parse(time);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getSk_time_str() {
@@ -37,5 +54,13 @@ public class ScheduleVO {
 		}
 		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
 		return format.format(sk_time);
+	}
+	public void setSk_time(String time) {
+		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
+		try {
+			sk_time = format.parse(time);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
